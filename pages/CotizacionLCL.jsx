4a8254994b1ced222cf9308b8ed28b4dfcf42a28
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2023-04-13 09:22:09 -0400

    Update CotizacionLCL.jsx
    Cc

--- pages/CotizacionLCL.jsx
+++ pages/CotizacionLCL.jsx
@@ -1,28 +1,232 @@
 import { useRouter } from 'next/router'
 import Image from 'next/image'
+import { useReducer, useState } from 'react'
 import { useUser } from '../context/Context'
 import { WithAuth } from '../HOCs/WithAuth'
 import Layout from '../layout/Layout'
 import Card from '../components/Card'
+import ReactPDF from '@react-pdf/renderer';
 
-import style from '../styles/Admin.module.css'
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
 
-function Admin() {
-    const { user } = useUser()
+
+import style from '../styles/CotizacionTerrestre.module.css'
+import Button from '../components/Button'
+
+
+function CotizacionTerrestre() {
+    const { user, pdfData, setUserPdfData } = useUser()
     const router = useRouter()
+ 
+    const [tarifa, setTarifa] = useState([""])
+    const [otrosGastos, setOtrosGastos] = useState([""])
+    const [incluye, setIncluye] = useState([""])
 
+    const [calc, setCalc] = useState({})
+
+    function handleEventChange(e) {
+        setUserPdfData({ ...pdfData, ...{ [e.target.name]: e.target.value } })
+    }
+    function handlerCounter(word) {
+        const newTarifa = tarifa.map(i => i)
+        newTarifa.pop()
+        word == "pluss" ? setTarifa([...tarifa, ...[""]]) : setTarifa(newTarifa)
+    }
+    function handlerCounterTwo(word) {
+        const newTarifa = otrosGastos.map(i => i)
+        newTarifa.pop()
+        word == "pluss" ? setOtrosGastos([...otrosGastos, ...[""]]) : setOtrosGastos(newTarifa)
+    }
+    function handlerCounterThree(word) {
+        const newIncluye = incluye.map(i => i)
+        newIncluye.pop()
+        word == "pluss" ? setIncluye([...incluye, ...[""]]) : setIncluye(newIncluye)
+    }
+
+    function handlerPdfButton() {
+        router.push("/PdfView")
+    }
+
+
+
+    function handlerCalc(e, index) {
+
+        if (e.target.name == `CANTIDADFLETE${index}` && calc[`FLETEUNITARIO${index}`] !== undefined) {
+            let object = reducer(e, index, 'FLETEUNITARIO', 'PRODUCTFLETE', 'FLETETOTAL')
+            setCalc({ ...calc, ...object })
+            return
+        }
+
+        if (e.target.name == `FLETEUNITARIO${index}` && calc[`CANTIDADFLETE${index}`] !== undefined) {
+            let object = reducer(e, index, 'CANTIDADFLETE', 'PRODUCTFLETE', 'FLETETOTAL')
+            setCalc({ ...calc, ...object })
+            return
+        }
+
+        if (e.target.name == `CANTIDAD${index}` && calc[`COSTOUNITARIO${index}`] !== undefined) {
+            let object = reducer(e, index, 'COSTOUNITARIO', 'PRODUCT', 'TOTAL')
+            setCalc({ ...calc, ...object })
+            return
+        }
+
+        if (e.target.name == `COSTOUNITARIO${index}` && calc[`CANTIDAD${index}`] !== undefined) {
+            let object = reducer(e, index, 'CANTIDAD', 'PRODUCT', 'TOTAL')
+            setCalc({ ...calc, ...object })
+            return
+        }
+
+        let object = {
+            [e.target.name]: e.target.value,
+
+        }
+        setCalc({ ...calc, ...object })
+    }
+
+
+    function reducer (e, index, counter, prod, total) {
+        let product = e.target.value * calc[`${counter}${index}`]
+
+        let data = {
+            ...calc,
+            [e.target.name]: e.target.value,
+            [`${prod}${index}`]: product,
+        }
+
+        let arr = Object.entries(data)
+
+        let red = arr.reduce((ac, i) => {
+            let str = i[0]
+            if (str.includes(total)) {
+                return ac
+            }
+            if (prod == 'PRODUCT' && str.includes('PRODUCTFLETE')) {
+                return ac
+            }
+            let res = str.includes(prod)
+            let r = res ? i[1] + ac : ac
+            return r
+        }, 0)
+
+        let object = {
+            [e.target.name]: e.target.value,
+            [`${prod}${index}`]: product,
+            PRODUCTOFLETETOTAL: prod === 'PRODUCTFLETE' ? red : data['PRODUCTOFLETETOTAL'],
+            PRODUCTOTOTAL: prod === 'PRODUCT' ? red : data['PRODUCTOTOTAL'],
+        }
+        return object
+    }
+
+    console.log(calc)
 
     return (
         <Layout>
-
             <div className={style.container}>
-                <Image src="/logo.svg" width="350" height="250" alt="User" />
-            </div>
-            <div className={style.imgContainer}>
-                Estamos trabajando...
-            </div>        </Layout>
+                <form className={style.form}>
+                    <div className={style.subtitle}>COTIZACIÓN TRANSPORTE TERRESTRE</div>
+                    <div className={style.containerFirstItems}>
+                        <div className={style.imgForm}>
+                            <Image src="/logo.svg" width="250" height="150" alt="User" />
+                        </div>
+                        <div className={style.firstItems}>
+                            <div>
+                                <label htmlFor="">COTIZACIÓN No</label>
+                                <input type="text" name={"COTIZACIÓN No"} onChange={handleEventChange} />
+                            </div>
+                            <div>
+                                <label htmlFor="">FECHA</label>
+                                <input type="text" name={"FECHA"} onChange={handleEventChange} />
+                            </div>
+                            <div>
+                                <label htmlFor="">VALIDEZ</label>
+                                <input type="text" name={"VALIDEZ"} onChange={handleEventChange} />
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                    <div className={style.subtitle}>DATOS DE CLIENTE</div>
+                    <br />
+                    <div className={style.items}>
+                        <div>
+                            <label htmlFor="">NOMBRE</label>
+                            <input type="text" name={"NOMBRE"} onChange={handleEventChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="">CORREO</label>
+                            <input type="text" name={"CORREO"} onChange={handleEventChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="">EMPRESA</label>
+                            <input type="text" name={"EMPRESA"} onChange={handleEventChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="">TELEFONO</label>
+                            <input type="text" name={"TELEFONO"} onChange={handleEventChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="">CARGO</label>
+                            <input type="text" name={"CARGO"} onChange={handleEventChange} />
+                        </div>
 
+                        <div>
+                            <label htmlFor="">CIUDAD</label>
+                            <input type="text" name={"CIUDAD"} onChange={handleEventChange} />
+                        </div>
+                    </div>
+                    <br />
+                    <div className={style.subtitle}>DESCRIPCION DE SERVICIO</div>
+                    <br />
+                    <div className={style.items}>
+                        <div>
+                            <label htmlFor="">NÚMERO DE SERVICIO</label>
+                            <input type="text" name={"MERCANCIA"} onChange={handleEventChange} />
+                        </div>
+                        
+                        <div>
+                            <label htmlFor="">MONEDA</label>
+                            <input type="text" name={"VOLUMEN M3"} onChange={handleEventChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="">MERCANCÍA</label>
+                            <input type="text" name={"PESO TN"} onChange={handleEventChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="">TIPO DE CAMBIO</label>
+                            <input type="text" name={"CANTIDAD"} onChange={handleEventChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="">TIPO DE CARGA</label>
+                            <input type="text" name={"MERCANCIA"} onChange={handleEventChange} />
+                        </div>
+                        
+                        <div>
+                            <label htmlFor="">CONDICIONES DE PAGO</label>
+                            <input type="text" name={"VOLUMEN M3"} onChange={handleEventChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="">TIPO DE SERVICIO</label>
+                            <input type="text" name={"PESO TN"} onChange={handleEventChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="">CONTRATO/COTIZACIÓN</label>
+                            <input type="text" name={"CANTIDAD"} onChange={handleEventChange} />
+                        </div>
+                        
+                    </div>
+                    <br />
+
+                    
+
+  
+
+                </form>
+            </div>
+            <button className={style.downloadPDF} onClick={handlerPdfButton}>
+                <Image src="/download-pdf.svg" width="50" height="50" alt="User" />
+            </button>
+            <br />
+            <br />
+        </Layout>
     )
 }
 
-export default WithAuth(Admin)
+export default WithAuth(CotizacionTerrestre) 
